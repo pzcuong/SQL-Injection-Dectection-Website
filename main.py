526d5ebed0940e39101fdc3d21124f9681e52ca8
pzcuong <dok2308@gmail.com>
2022-12-04 10:32:20 +0700

    fix lib require

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, render_template
 import pickle
-from tensorflow import keras
+# from tensorflow import keras
 import library
 
 app = Flask(__name__, template_folder='public', static_folder='public')
 vectorizer = pickle.load(open('./model/vectorizer.pickle', 'rb'))
 rf_model = pickle.load(open('./model/rf_clf.pickle', 'rb'))
 
-model = keras.models.load_model('./model/CNN_Model')
+# model = keras.models.load_model('./model/CNN_Model')
 
 @app.route('/')
 def main_page():
